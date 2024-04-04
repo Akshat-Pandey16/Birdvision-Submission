5067ae69996c1a7102a9628b02491dd55e1b6a1e
Akshat Pandey <aplinkinpark16@gmail.com>
2024-04-04 19:21:01 +0530

    initialized all apis with basic content

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    name="Birdvision Submission",
+    description="Product apis to manage products",
+    version="1.0.0",
+)
+
 
 @app.get("/")
 def index():
     return {"Hello": "World"}
+
+
+@app.get("/products")
+def get_products():
+    return {"Api to get all products"}
+
+
+@app.get("/products/{id}")
+def get_products_by_id():
+    return {"Api to get product of an id"}
+
+
+@app.post("/products")
+def create_product():
+    return {"Api to create a product"}
+
+
+@app.put("/products/{id}")
+def update_product():
+    return {"Api to update product of an id"}
+
+
+@app.delete("/products/{id}")
+def delete_product():
+    return {"Api to delete product of an id"}
+
+
+@app.delete("/products")
+def delete_all_products():
+    return {"Api to delete all products"}
