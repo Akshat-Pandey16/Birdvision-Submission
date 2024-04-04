00c2aa744ddee19a0f3e64a69d33ab3c93164080
Akshat Pandey <aplinkinpark16@gmail.com>
2024-04-04 20:35:38 +0530

    added get product by id

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 from db import create_database, Product, get_db
 from models import ProductModel
-import json
 
 
 app = FastAPI(
-    name="Birdvision Submission",
+    title="Birdvision Submission",
     description="Product APIs to manage products",
     version="1.0.0",
 )
@@ -22,28 +21,36 @@ def index():
 @app.get("/products")
 def get_products(db: Session = Depends(get_db)):
     products = db.query(Product).offset(0).limit(5).all()
-    return {"products": json(products)}
+    return {"products": products}
 
 
 @app.get("/products/{id}")
-def get_products_by_id(db: Session = Depends(get_db)):
+def get_products_by_id(id: int, db: Session = Depends(get_db)):
     product = db.query(Product).filter(Product.id == id).first()
     if product:
         return product
 
 
 @app.post("/products")
-def create_product():
-    return {"Api to create a product"}
+def create_product(Products: ProductModel, db: Session = Depends(get_db)):
+    title = Products.title
+    description = Products.description
+    price = Products.price
+    count = Products.count
+    product = Product(title=title, description=description, price=price, count=count)
+    db.add(product)
+    db.commit()
+    db.refresh(product)
+    return product
 
 
 @app.put("/products/{id}")
-def update_product():
+def update_product(id: int):
     return {"Api to update product of an id"}
 
 
 @app.delete("/products/{id}")
-def delete_product():
+def delete_product(id: int):
     return {"Api to delete product of an id"}
 
 
