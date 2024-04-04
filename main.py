bfea542a4415b4c4dea7b0c6d02e8c3258f0e1b7
Akshat Pandey <aplinkinpark16@gmail.com>
2024-04-04 20:10:51 +0530

    added get all products and delete all products functionality

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from db import create_database, Product, get_db
+from models import ProductModel
+import json
+
 
 app = FastAPI(
     name="Birdvision Submission",
-    description="Product apis to manage products",
+    description="Product APIs to manage products",
     version="1.0.0",
 )
+
+create_database()
 
 
 @app.get("/")
 def index():
-    return {"Hello": "World"}
+    return {"message": "Go to /docs to view the APIs documentation."}
 
 
 @app.get("/products")
-def get_products():
-    return {"Api to get all products"}
+def get_products(db: Session = Depends(get_db)):
+    products = db.query(Product).offset(0).limit(5).all()
+    return {"products": json(products)}
 
 
 @app.get("/products/{id}")
-def get_products_by_id():
-    return {"Api to get product of an id"}
+def get_products_by_id(db: Session = Depends(get_db)):
+    return {"Api to get product for an id"}
 
 
 @app.post("/products")
@@ -38,5 +46,13 @@ def delete_product():
 
 
 @app.delete("/products")
-def delete_all_products():
-    return {"Api to delete all products"}
+def delete_all_products(db: Session = Depends(get_db)):
+    try:
+        db.query(Product).delete()
+        db.commit()
+        return {"message": "Database flushed successfully"}
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=500, detail=f"Error flushing database: {str(e)}"
+        )
